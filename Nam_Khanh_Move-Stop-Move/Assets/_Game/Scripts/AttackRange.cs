593113bc45916e80af3caef423d53f4b162fd8c2
Kevin <kn6267658@gmail.com>
2025-10-22 17:24:31 +0700

    + New Asset

---
+++ Nam_Khanh_Move-Stop-Move/Assets/_Game/Scripts/AttackRange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackRange : MonoBehaviour
+{
+    [SerializeField] private Transform rangeCircle;
+    [SerializeField] private float attackRange = 3f;
+
+    void Start()
+    {
+        UpdateRangeCircle();
+        ShowRange(true);
+    }
+
+    public void UpdateRangeCircle()
+    {
+        if (rangeCircle != null)
+        {
+            float scale = attackRange / 5f;
+            rangeCircle.localScale = new Vector3(10, 10, scale);
+        }
+    }
+
+    public void ShowRange(bool show)
+    {
+        if (rangeCircle != null)
+            rangeCircle.gameObject.SetActive(show);
+    }
+}
